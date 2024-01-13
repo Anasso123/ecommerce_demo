c8ba9cd47bd9a314f262fb8f8c94bc25ab1f68ff
anasso123 <haimeuranass4@gmail.com>
2024-01-13 16:32:34 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/anasshaimeur/ecommerce/e_commerce_design/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.anasshaimeur.ecommerce.e_commerce_design
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/src/model/product_size_type.dart
@@ -0,0 +1,11 @@
+
+
+import 'package:e_commerce_design/src/model/categorical.dart';
+import 'package:e_commerce_design/src/model/numerical.dart';
+
+class ProductSizeType {
+  List<Numerical>? numerical;
+  List<Categorical>? categorical;
+
+  ProductSizeType({this.numerical, this.categorical});
+}
